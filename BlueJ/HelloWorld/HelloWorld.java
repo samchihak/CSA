3a46d9e24b6a70a2aa5734f75a80a1f2eebfe631
Sam Chihak <samchihak17@gmail.com>
2018-09-05 13:11:12 -0500

    1.1.1 - 1
    Did steps 1-15 of worksheet 1.1.1

---
+++ BlueJ/HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Practice program to learn Java
+ *
+ * @author Sam Chihak
+ * @version 9/5/2018
+ */
+public class HelloWorld
+ {
+    public static void main ()
+     {
+         System.out.println("Hello, World!");
+      
+     }  
+    
+ }    
