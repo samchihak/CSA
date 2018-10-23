5daeb1b25f24ec82a9d48898a24cc5ceae5c1305
Sam Chihak <samchihak17@gmail.com>
2018-10-22 20:16:11 -0500

    did the same for books/films
    basically just copy/pasted and found/replaced songs w books/films. Haven't tested but it should be solid.

--- BlueJ/MediaLib/FilmInput.java
+++ BlueJ/MediaLib/FilmInput.java
@@ -1,14 +1,48 @@
-
+import java.util.*;
+import java.io.*;
 /**
- * Write a description of class FilmInput here.
+* Attempting to create a function that allows user to input films to MediaLib
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Sam Chihak & Rivers
+ * @version 10/22
  */
 public class FilmInput
 {
-    public FilmInput()
-    {
 
+    public static void filmInput(){
+        String title;
+        int rating;
+        int duration;
+        double price;
+        int x = 0;
+        System.out.println("Enter some of your favorite films.");
+        System.out.println(" ");
+        Scanner numfilms = new Scanner(System.in);
+        System.out.println("How many films do you want to enter?");
+        int nums = numfilms.nextInt();
+        numfilms.close();
+        ArrayList<Film> userfilms = new ArrayList<Film>(nums-1);
+        while(x<(nums)){
+
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("What's the name of the film?");
+            title = userInput.nextLine();
+            System.out.println("What would you rate it out of 10?");
+            rating = userInput.nextInt();
+            System.out.println("How many pages is it?");
+            duration = userInput.nextInt();
+            System.out.println("How much does it cost?");
+            price = userInput.nextDouble();
+
+            // Close the scanner object using close() method to prevent memory leak
+            userInput.close();
+            Film userFilm = new Film(title, rating, duration, price);
+            userfilms.add(userFilm);
+            x += 1;
+            System.out.println("__________________________");
+        }
+        System.out.println("Here are your favorite films:");
+        for (int i=0; i<userfilms.size(); i++)
+            userfilms.get(i).pint(); 
     }
 }
